196256d40090e7bcc92634ac45a5cbb696e8e2bd
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-11-19 14:55:26 +0600

    Improved skills section and dynamic age

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 setInterval(() => {
-  document.querySelector(".clock").innerHTML = new Date().toLocaleTimeString()
+  document.querySelector(".clock").innerHTML = new Date().toLocaleTimeString();
 }, 1000);
+
+currYear = new Date().getFullYear();
+age = currYear - 2011;
+document.getElementById("age").innerHTML = age;
