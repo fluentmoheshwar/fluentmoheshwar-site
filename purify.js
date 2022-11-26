d670a2372f649277ac60dbfa58acc7edf81f7fd9
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-11-26 12:21:00 +0600

    Removed PurifyCSS

--- purify.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import purify from "purify-css";
-
-const content = ["**/dist/*.html"];
-const css = ["**/dist/assets/*.css"];
-const options = { minify: true };
-purify(content, css, options);
