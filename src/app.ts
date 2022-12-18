2088588cadfce44bb363b9d3ee6fba5c3df87f1e
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-12-18 18:41:18 +0600

    Added tailwind

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,5 @@
-/* eslint-disable */
-import * as bootstrap from "bootstrap";
-/* eslint-enable */
-
-setInterval(() => {
-  document.querySelector(".clock").innerHTML = new Date().toLocaleTimeString(
-    "en-US"
-  );
-}, 1000);
-const currYear = new Date().getFullYear();
-const age = currYear - 2011;
-document.getElementById("age").innerHTML = age;
-navigator.serviceWorker.register("/service-worker.js");
+const currYear: number = new Date().getFullYear();
+const age: number = currYear - 2011;
+document.getElementById("age")?.innerHTML = age;
+// navigator.serviceWorker.register("/service-worker.js");
+export {};
