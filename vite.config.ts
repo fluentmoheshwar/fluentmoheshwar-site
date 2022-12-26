c8840e4bb8da2016b1c86490b0eddb3408b1a6c2
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-12-26 17:07:45 +0600

    Removed pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,68 +1,7 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import webfontDownload from "vite-plugin-webfont-dl";
 export default defineConfig({
   publicDir: "public",
   server: { port: 3000 },
-  plugins: [
-    VitePWA({
-      injectRegister: "auto",
-      includeAssets: ["/favicon.ico", "/favicon.png"],
-      manifest: {
-        name: "Moheshwar Amarnath Biswas",
-        short_name: "Moheshwar Amarnath Biswas",
-        start_url: "./index.html",
-        display: "standalone",
-        description: "Website of Moheshwar Amarnath Biswas",
-        lang: "en",
-        dir: "ltr",
-        theme_color: "#111827",
-        background_color: "#202020",
-        orientation: "any",
-        icons: [
-          {
-            src: "./favicon512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "./favicon.png",
-            sizes: "424x424",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "./favicon192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-        ],
-        screenshots: [
-          {
-            src: "/phone.png",
-            sizes: "1170x2532",
-            type: "image/png",
-          },
-          {
-            src: "/meta.png",
-            sizes: "3840x2160",
-            type: "image/png",
-          },
-        ],
-        shortcuts: [
-          {
-            name: "Home",
-            url: "/",
-            description: "The Home page",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-    }),
-    webfontDownload(),
-  ],
+  plugins: [webfontDownload()],
 });
